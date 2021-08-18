216f7f22901b9ca722847b254952480c5983b383
Benji Thompson <thompson.benji@gmail.com>
2021-08-18 17:13:23 -0400

    can fetch data for specific address

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
 import SignInForm from './components/SignInForm'
 
+const baseURL = 'https://jobcoin.gemini.com/greyhound-abruptly/api/'
+
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
       isLoggedIn: false,
-      loggedInAddress: ''
+      loggedInAddress: '',
+      data: ''
     }
+
+    this.handleLogin = this.handleLogin.bind(this)
+    this.getData = this.getData.bind(this)
+  }
+
+  handleLogin(address) {
+    this.setState({
+      isLoggedIn: true,
+      loggedInAddress: address
+    })
+  }
+
+  getData(address) {
+    fetch(baseURL + 'addresses/' + address)
+      .then(data => { return data.json()}, err => console.log(err))
+      .then(parsedData => this.setState({ data: parsedData }), err => console.log(err))
   }
 
   render() {
     return (
       <div className="container text-center mt-5">
         <h1>JOBCOIN</h1>
-        <SignInForm />
+        <SignInForm
+          handleLogin={ this.handleLogin }
+          getData={ this.getData } />
       </div>
     )
   }
