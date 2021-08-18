ef14ece88f31cf18b1b23660326c6ecde86d53ee
Benji Thompson <thompson.benji@gmail.com>
2021-08-18 16:11:58 -0400

    refactored SignInForm into own component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react'
+import SignInForm from './components/SignInForm'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container text-center mt-5">
         <h1>JOBCOIN</h1>
-        <div className="mt-5">
-          <h5>Welcome! Please sign in with your JOBCOIN address.</h5>
-          <form className="mt-4">
-            <div className="row g-3 align-items-center justify-content-center">
-              <div className="col-auto">
-                <label htmlFor="address" className="form-label">Address</label>
-              </div>
-              <div className="col-auto">
-                <input type="text" className="form-control" id="address" placeholder="Jilly" />
-              </div>
-              <div className="col-auto">
-                <button type="submit" className="btn btn-primary">Sign In</button>
-              </div>
-            </div>
-          </form>
-        </div>
+        <SignInForm />
       </div>
     )
   }
---
+++ src/components/SignInForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class SignInForm extends Component {
+    render() {
+        return (
+            <div className="mt-5">
+                <h5>Welcome! Please sign in with your JOBCOIN address.</h5>
+                <form className="mt-4">
+                    <div className="row g-3 align-items-center justify-content-center">
+                    <div className="col-auto">
+                        <label htmlFor="address" className="form-label">Address</label>
+                    </div>
+                    <div className="col-auto">
+                        <input type="text" className="form-control" id="address" placeholder="Jilly" />
+                    </div>
+                    <div className="col-auto">
+                        <button type="submit" className="btn btn-primary">Sign In</button>
+                    </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
