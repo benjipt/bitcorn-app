0b00af24bdb728dc805c3f654bd0e21c321a40ef
Benji Thompson <thompson.benji@gmail.com>
2021-08-19 14:04:15 -0400

    updated layout of signout button

--- src/components/SignOutBtn.js
+++ src/components/SignOutBtn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class SignOutBtn extends Component {
     render() {
         return (
-            <div className="container text-center mt-5">
+            <div className="container-fluid text-end mt-2">
                 <button onClick={ this.props.handleLogout } className="btn btn-outline-dark">Sign Out</button>
             </div>
         )
