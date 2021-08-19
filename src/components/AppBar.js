1d73b9136506a6c18410e6ed40a4d4a42bc0a4ad
Benji Thompson <thompson.benji@gmail.com>
2021-08-19 16:57:47 -0400

    styling updates

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,7 +4,8 @@ import SignOutBtn from './SignOutBtn'
 export default class AppBar extends Component {
     render() {
         return (
-            <div className="container-fluid pt-2 row align-items-center">
+            <div className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-end app-bar app-bar-border">
+
                 <SignOutBtn handleLogout={ this.props.handleLogout } />
             </div>
         )
--- src/components/SignOutBtn.js
+++ src/components/SignOutBtn.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class SignOutBtn extends Component {
     render() {
         return (
-            <div className="">
+            <div>
                 <button onClick={ this.props.handleLogout } className="btn btn-outline-dark">Sign Out</button>
             </div>
         )
