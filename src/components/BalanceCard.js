268995425e320c11375ab57a9ae3472b7ca03363
Benji Thompson <thompson.benji@gmail.com>
2021-09-16 11:30:18 -0400

    refactored BalanceCard from class to function

--- src/components/BalanceCard.js
+++ src/components/BalanceCard.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class BalanceCard extends Component {
-    render() {
-        return (
-            <div className="container text-center mt-5 p-4 border-black rounded card-custom">
-                <h5>JobCoin Balance</h5>
-                <hr></hr>
-                <p className="balance">{this.props.balance}</p>
-            </div>
-        )
-    }
+export default function BalanceCard({ balance }) {
+    return (
+        <div className="container text-center mt-5 p-4 border-black rounded card-custom">
+            <h5>JobCoin Balance</h5>
+            <hr></hr>
+            <p className="balance">{ balance }</p>
+        </div>
+    )
 }
