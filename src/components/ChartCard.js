0c357950eb8ca0f4699699f5ed85b132701274c6
Benji Thompson <thompson.benji@gmail.com>
2021-09-16 11:32:05 -0400

    refactored ChartCard from class to function

--- src/components/ChartCard.js
+++ src/components/ChartCard.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Tooltip, XAxis, YAxis, ResponsiveContainer, LineChart, Line } from 'recharts'
 
-export default class ChartCard extends Component {
-    render() {
-        return (
-            <div className="container mt-5 p-4 border-black rounded card-custom">
-                <div className="text-center">
-                    <h5>Balance History</h5>
-                </div>
-                <hr></hr>
-                <ResponsiveContainer width="100%" height={550}>
-                    <LineChart data={ this.props.data } className="linechart-style" >
-                        <XAxis dataKey="date" />
-                        <YAxis />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="amount" stroke="#FF3366" strokeWidth={4} />
-                    </LineChart>
-                </ResponsiveContainer>
+export default function ChartCard({ data }) {
+    return (
+        <div className="container mt-5 p-4 border-black rounded card-custom">
+            <div className="text-center">
+                <h5>Balance History</h5>
             </div>
-        )
-    }
+            <hr></hr>
+            <ResponsiveContainer width="100%" height={550}>
+                <LineChart data={ data } className="linechart-style" >
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="amount" stroke="#FF3366" strokeWidth={4} />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
 }
