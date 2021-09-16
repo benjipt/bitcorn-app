69e07f7de4f88f7879febd41941c509b8fd0c68a
Benji Thompson <thompson.benji@gmail.com>
2021-09-16 12:05:52 -0400

    refactored SignInPage from class to function

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,55 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class SignInPage extends Component {
-    constructor(props) {
-        super(props)
+export default function SignInPage({ getData }) {
 
-        this.state = {
-            address: '',
-            submitError: false
-        }
+    // STATE HOOKS
+    const [ addressInput, setAddressInput ] = useState('')
+    const [ submitError, setSubmitError ] = useState(false)
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+    const handleChange = e => {
+        const { value } = e.currentTarget
+        setAddressInput(value)
     }
 
-    handleChange(event) {
-        this.setState({ [event.currentTarget.id]: event.currentTarget.value })
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-        if (!this.state.address) {
-            this.setState({ submitError: true })
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (!addressInput) {
+            setSubmitError(true)
         } else {
-            this.props.getData(this.state.address)
+            getData(addressInput)
         }
     }
 
-    render() {
-        return (
-            <div className="container text-center mt-5">
-                <h1>JOBCOIN</h1>
-                <div className="mt-5 p-4 border-black rounded sign-in">
-                    <p>Welcome! Please sign in with your JobCoin address</p>
-                    <hr></hr>
-                    <form onSubmit={ this.handleSubmit } className="mt-4">
-                        <div className="row g-3 align-items-center justify-content-center">
-                            <div className="col-auto">
-                                <label htmlFor="address" className="form-label visually-hidden">Address</label>
-                            </div>
-                            <div className="col-auto">
-                                <input onChange={ this.handleChange } type="text" className="form-control" id="address" placeholder="Jilly" />
-                            </div>
-                            <div className="col-auto">
-                                <button type="submit" className="btn btn-primary">Sign In</button>
-                            </div>
-                            { this.state.submitError && 
-                                <div id="submitError" className="form-text pt-2">Must enter an address to sign in</div> }
+    return (
+        <div className="container text-center mt-5">
+            <h1>JOBCOIN</h1>
+            <div className="mt-5 p-4 border-black rounded sign-in">
+                <p>Welcome! Please sign in with your JobCoin address</p>
+                <hr></hr>
+                <form onSubmit={ handleSubmit } className="mt-4">
+                    <div className="row g-3 align-items-center justify-content-center">
+                        <div className="col-auto">
+                            <label htmlFor="address" className="form-label visually-hidden">Address</label>
                         </div>
-                    </form>
-                </div>
+                        <div className="col-auto">
+                            <input onChange={ handleChange } type="text" className="form-control" id="address" placeholder="Jilly" value={ addressInput } />
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary">Sign In</button>
+                        </div>
+                        { submitError && 
+                            <div id="submitError" className="form-text pt-2">Must enter an address to sign in</div> }
+                    </div>
+                </form>
             </div>
-        )
-    }
+        </div>
+    )
 }
