01d43e77231fe3b8c6e611e67d8e16c35d96c51d
Benji Thompson <thompson.benji@gmail.com>
2021-09-16 11:28:23 -0400

    refactored AppBar from class to function

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AddressName from './AddressName'
 import SignOutBtn from './SignOutBtn'
 
-export default class AppBar extends Component {
-    render() {
-        return (
-            <div className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-between app-bar app-bar-border">
-                <AddressName address={ this.props.address } />
-                <SignOutBtn handleLogout={ this.props.handleLogout } />
-            </div>
-        )
-    }
+export default function AppBar({ address, handleLogout }) {
+    return (
+        <div className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-between app-bar app-bar-border">
+            <AddressName address={ address } />
+            <SignOutBtn handleLogout={ handleLogout } />
+        </div>
+    )
 }
