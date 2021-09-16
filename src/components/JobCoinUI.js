b29d3fed231a90e823711a653bdbf98a9e43a6b4
Benji Thompson <thompson.benji@gmail.com>
2021-09-16 11:34:31 -0400

    refactored JobCoinUI from class to function

--- src/components/JobCoinUI.js
+++ src/components/JobCoinUI.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AppBar from './AppBar'
 import BalanceCard from './BalanceCard'
 import SendCard from './SendCard'
 import ChartCard from './ChartCard'
 
-export default class JobCoinUI extends Component {
-    render() {
-        return (
-            <div>
-                <AppBar 
-                    address={ this.props.address }
-                    handleLogout={ this.props.handleLogout } />
-                <div className="container-fluid">
-                    <div className="row ps-3">
-                        <div className="row col-lg-4 me-3">
-                            <BalanceCard 
-                                balance={ this.props.balance } />
-                            <SendCard 
-                                address={ this.props.address }
-                                getData={ this.props.getData } />
-                        </div>
-                        <div className="row col-lg-8">
-                            <ChartCard 
-                                data={ this.props.data } />
-                        </div>
+export default function JobCoinUI({ address, handleLogout, balance, getData, data }) {
+    return (
+        <div>
+            <AppBar 
+                address={ address }
+                handleLogout={ handleLogout } />
+            <div className="container-fluid">
+                <div className="row ps-3">
+                    <div className="row col-lg-4 me-3">
+                        <BalanceCard 
+                            balance={ balance } />
+                        <SendCard 
+                            address={ address }
+                            getData={ getData } />
+                    </div>
+                    <div className="row col-lg-8">
+                        <ChartCard 
+                            data={ data } />
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
