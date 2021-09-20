da376e9bf20de14c65259a1b22f5698411d71159
Benji Thompson <thompson.benji@gmail.com>
2021-09-20 15:00:46 -0400

    refactored App from class to function, resolved issue where balance history chart registered only negative balance values

--- src/App.js
+++ src/App.js
@@ -1,62 +1,51 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { format } from 'date-fns'
 import SignInPage from './components/SignInPage'
 import JobCoinUI from './components/JobCoinUI'
 
 const baseURL = 'https://jobcoin.gemini.com/greyhound-abruptly/api/'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
+export default function App() {
+  // STATE HOOKS
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+  const [ loggedInAddress, setLoggedInAddress ] = useState('')
+  const [ balance, setBalance ] = useState('')
+  const [ runningBalance, setRunningBalance ] = useState([])
 
-    this.state = {
-      isLoggedIn: false,
-      loggedInAddress: '',
-      balance: '',
-      runningBalance: []
-    }
-
-    this.getData = this.getData.bind(this)
-    this.setRunningBalance = this.setRunningBalance.bind(this)
-    this.generatePlot = this.generatePlot.bind(this)
-    this.updateBalanceArr = this.updateBalanceArr.bind(this)
-    this.handleLogout = this.handleLogout.bind(this)
-  }
-
-  getData(address) {
+  // RETRIEVES ADDRESS DATA FROM API
+  const getData = address => {
     fetch(baseURL + 'addresses/' + address)
       .then(data => { return data.json()}, err => console.log(err))
       .then(parsedData => {
-        this.setState({ 
-        isLoggedIn: true,
-        loggedInAddress: address,
-        balance: parsedData.balance,
-        transactions: parsedData.transactions })
-        this.setRunningBalance(this.state.transactions)
+        const { balance, transactions } = parsedData
+        setIsLoggedIn(true)
+        setLoggedInAddress(address)
+        setBalance(balance)
+        createRunningBalance(transactions, address)
       }, err => console.log(err))
   }
-
-  setRunningBalance(transactions) {
+  // SHAPES DATA FOR BALANCE HISTORY CHART ~~~~~>
+  const createRunningBalance = (transactions, loggedInAddress) => {
     let currentBalance = 0
     let balanceArr = []
     for (let transaction of transactions) {
-      if (transaction.toAddress === this.state.loggedInAddress) {
+      if (transaction.toAddress === loggedInAddress) {
         currentBalance += Number(transaction.amount)
       } else {
         currentBalance -= Number(transaction.amount)
       }
-      const plot = this.generatePlot(transaction, currentBalance)
-      this.updateBalanceArr(plot, balanceArr)
+      const plot = generatePlot(transaction, currentBalance)
+      updateBalanceArr(plot, balanceArr)
     }
-    this.setState({ runningBalance: balanceArr })
+    setRunningBalance(balanceArr)
   }
-
-  generatePlot(transaction, currentBalance) {
+  // Formats dates for chart rendering
+  const generatePlot = (transaction, currentBalance) => {
     const formattedDate = format(new Date(transaction.timestamp), 'yyyy-MM-dd')
     return { amount: currentBalance, date: formattedDate }
   }
-
-  updateBalanceArr(plot, balanceArr) {
+  // Ensures balance value from each day is latest balance
+  const updateBalanceArr = (plot, balanceArr) => {
     const lastIndex = balanceArr.length - 1
     if (lastIndex > -1) {
       if (plot.date === balanceArr[lastIndex].date) {
@@ -68,32 +57,29 @@ export default class App extends Component {
       balanceArr.push(plot)
     }
   }
+  // <~~~~~ SHAPES DATA FOR BALANCE HISTORY CHART
 
-  handleLogout() {
-    this.setState({
-      isLoggedIn: false,
-      loggedInAddress: '',
-      balance: '',
-      transactions: []
-    })
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+    setLoggedInAddress(false)
+    setBalance('')
+    setRunningBalance([])
   }
 
-  render() {
-    return (
-      <div>
-        { !this.state.isLoggedIn && 
-          <SignInPage
-            getData={ this.getData } /> }
+  return (
+    <div>
+      { !isLoggedIn && 
+        <SignInPage
+          getData={ getData } /> }
 
-        { this.state.isLoggedIn &&
-          <JobCoinUI 
-            handleLogout={ this.handleLogout }
-            address={ this.state.loggedInAddress }
-            balance={ this.state.balance }
-            getData={ this.getData }
-            data={ this.state.runningBalance } /> }
-      </div>
-    )
-  }
+      { isLoggedIn &&
+        <JobCoinUI 
+          handleLogout={ handleLogout }
+          address={ loggedInAddress }
+          balance={ balance }
+          getData={ getData }
+          data={ runningBalance } /> }
+    </div>
+  )
 }
 
