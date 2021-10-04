1aa6808ceb95f20a1aaa4710fdfcd11007e12a21
Benji Thompson <thompson.benji@gmail.com>
2021-10-04 17:43:17 -0400

    added cleanup to unit test for AddressName

--- src/components/__tests__/AddressName.test.js
+++ src/components/__tests__/AddressName.test.js
@@ -1,6 +1,10 @@
 import { render, screen, cleanup } from '@testing-library/react'
 import AddressName from '../AddressName'
 
+afterEach(() => {
+    cleanup()
+})
+
 test('should render AddressName component with address prop', () => {
     const address = 'Jilly'
     render(<AddressName address={ address } />)
