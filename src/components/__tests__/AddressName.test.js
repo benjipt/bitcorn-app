c143c7121eb8755d80b6e0f4abd333e4d00e87da
Benji Thompson <thompson.benji@gmail.com>
2021-10-04 18:15:08 -0400

    added snapshot test for AddressName

--- src/components/__tests__/AddressName.test.js
+++ src/components/__tests__/AddressName.test.js
@@ -1,4 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react'
+import renderer from 'react-test-renderer'
 import AddressName from '../AddressName'
 
 afterEach(() => {
@@ -11,4 +12,10 @@ test('should render AddressName component with address prop', () => {
     const AddressNameComponent = screen.getByTestId('AddressName-1')
     expect(AddressNameComponent).toBeInTheDocument()
     expect(AddressNameComponent).toHaveTextContent('Jilly')
+})
+
+test('matches snapshot', () => {
+    const address = 'Jilly'
+    const tree = renderer.create(<AddressName address={ address } />).toJSON()
+    expect(tree).toMatchSnapshot()
 })
