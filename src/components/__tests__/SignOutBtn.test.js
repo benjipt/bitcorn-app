c1399cbab66e1af4d63aa683d9542a538ffd7066
Benji Thompson <thompson.benji@gmail.com>
2021-10-05 22:03:46 -0400

    added unit and snapshot test for SignOutBtn

---
+++ src/components/__tests__/SignOutBtn.test.js
@@ -0,0 +1,18 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import SignOutBtn from '../SignOutBtn'
+
+afterEach(() => {
+    cleanup()
+})
+
+test('should render SignOutButton component', () => {
+    render(<SignOutBtn />)
+    const SignOutBtnComponent = screen.getByTestId('SignOutBtn-1')
+    expect(SignOutBtnComponent).toBeInTheDocument()
+})
+
+test('matches snapshot', () => {
+    const tree = renderer.create(<SignOutBtn />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
