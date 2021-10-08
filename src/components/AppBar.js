2464e024de2243ffa91d1010aa6007286fb0fe7d
Benji Thompson <thompson.benji@gmail.com>
2021-10-07 21:18:47 -0400

    added unit test for AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,7 +4,9 @@ import SignOutBtn from './SignOutBtn'
 
 export default function AppBar({ address, handleLogout }) {
     return (
-        <div className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-between app-bar app-bar-border">
+        <div 
+            className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-between app-bar app-bar-border"
+            data-testid="AppBar-1" >
             <AddressName address={ address } />
             <SignOutBtn handleLogout={ handleLogout } />
         </div>
---
+++ src/components/__tests__/AppBar.test.js
@@ -0,0 +1,26 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import AppBar from '../AppBar'
+
+afterEach(() => {
+    cleanup()
+})
+
+describe('AppBar', () => {
+
+    test('should render AppBar with AddressName and SignOutBtn', () => {
+        const address = 'Jilly'
+        render(<AppBar address={ address } />)
+        const AppBarComponent = screen.getByTestId('AppBar-1')
+        const AddressNameComponent = screen.getByTestId('AddressName-1')
+        const SignOutBtnComponent = screen.getByTestId('SignOutBtn-1')
+        expect(AppBarComponent).toBeInTheDocument()
+        expect(AddressNameComponent).toBeInTheDocument()
+        expect(SignOutBtnComponent).toBeInTheDocument()
+    })
+
+    test('matches snapshot', () => {
+        const tree = renderer.create(<AppBar />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
