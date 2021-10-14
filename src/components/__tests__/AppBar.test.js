c1fb75366dd386e96ceea3aefe9b9a19d973fdbc
Benji Thompson <thompson.benji@gmail.com>
2021-10-14 16:12:36 -0400

    seperated and added to test for prop behavior in AppBar unit test

--- src/components/__tests__/AppBar.test.js
+++ src/components/__tests__/AppBar.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import AppBar from '../AppBar'
 
@@ -9,15 +9,31 @@ afterEach(() => {
 describe('AppBar', () => {
 
     test('should render AppBar with AddressName and SignOutBtn', () => {
-        const address = 'Jilly'
-        render(<AppBar address={ address } />)
+        // Renders Appbar and child components
+        render(<AppBar />)
+        // Grabs components from rendered tree
         const AppBarComponent = screen.getByTestId('AppBar-1')
         const AddressNameComponent = screen.getByTestId('AddressName-1')
         const SignOutBtnComponent = screen.getByTestId('SignOutBtn-1')
+        // Tests for expected components and content
         expect(AppBarComponent).toBeInTheDocument()
         expect(AddressNameComponent).toBeInTheDocument()
-        expect(AddressNameComponent).toHaveTextContent('Jilly')
         expect(SignOutBtnComponent).toBeInTheDocument()
+    })
+
+    test('passed props behave as expected', () => {
+        // Mock props
+        const address = 'Jilly'
+        const handleLogout = jest.fn()
+        // Renders Appbar and child components
+        render(<AppBar address={ address } handleLogout={ handleLogout } />)
+        // Grabs components from rendered tree
+        const AddressNameComponent = screen.getByTestId('AddressName-1')
+        const SignOutBtnComponent = screen.getByTestId('SignOutBtn-1')
+        // Tests for expected prop behavior
+        expect(AddressNameComponent).toHaveTextContent('Jilly')
+        fireEvent.click(SignOutBtnComponent)
+        expect(handleLogout).toHaveBeenCalled()
     })
 
     test('matches snapshot', () => {
