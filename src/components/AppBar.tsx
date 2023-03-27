b0e2f56e0d76cd54d9baef1d678350a08ad71847
Benji Thompson <thompson.benji@gmail.com>
2023-03-27 15:37:54 -0400

    refactor [AppBar.tsx]: convert from .js to .tsx

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 import AddressName from './AddressName'
 import SignOutBtn from './SignOutBtn'
 
-export default function AppBar({ address, handleLogout }) {
+interface AppBarProps {
+  address: string
+  handleLogout: () => void
+}
+
+export default function AppBar({ address, handleLogout }: AppBarProps) {
     return (
         <div 
             className="container-fluid pt-2 pb-2 d-flex flex-row justify-content-between app-bar app-bar-border"
