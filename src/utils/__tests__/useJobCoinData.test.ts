b63950c3c1d5569f7f5239ca3d8847ec5221357e
Benji Thompson <thompson.benji@gmail.com>
2023-04-04 21:52:59 -0400

    feat [useJobCoinData.ts]: add test

---
+++ src/utils/__tests__/useJobCoinData.test.ts
@@ -0,0 +1,40 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useJobCoinData } from '../useJobCoinData';
+import fetchMock from 'jest-fetch-mock';
+
+// Set up the fetch mock
+beforeAll(() => {
+  fetchMock.enableMocks();
+});
+
+// Reset the fetch mock after each test
+afterEach(() => {
+  fetchMock.resetMocks();
+});
+
+describe('useJobCoinData', () => {
+  test('should load persisted logged in address on mount', async () => {
+    // Arrange
+    sessionStorage.setItem('loggedInAddress', 'test-address');
+    const mockResponse = {
+      balance: '100',
+      transactions: [
+        {
+          amount: '50',
+          timestamp: '2023-04-01T12:00:00.000Z',
+          toAddress: 'test-address',
+        },
+      ],
+    };
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    // Act
+    const { result, waitForNextUpdate } = renderHook(() => useJobCoinData());
+
+    // Assert
+    await waitForNextUpdate();
+    expect(result.current.isLoggedIn).toBe(true);
+    expect(result.current.loggedInAddress).toBe('test-address');
+    expect(result.current.balance).toBe('100');
+  });
+});
