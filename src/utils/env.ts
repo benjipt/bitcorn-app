cf4da766220c0b4a657092fc11f0acdb3235924a
Benji Thompson <thompson.benji@gmail.com>
2023-04-28 21:43:53 -0400

    feat: add dynamic BASE_URL

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'https://jobcoin.gemini.com/greyhound-abruptly/api/';
+const isDevelopment = import.meta.env.MODE === 'development';
+
+export const BASE_URL = isDevelopment
+  ? 'http://localhost:3000/'
+  : 'https://jobcoin.gemini.com/greyhound-abruptly/api/';
