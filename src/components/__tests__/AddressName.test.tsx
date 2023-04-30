40881896cf4500f4b054e9b5cea8e18c9a55b852
Benji Thompson <thompson.benji@gmail.com>
2023-04-30 17:46:47 -0400

    feat[config]: use vitest instead of jest

--- src/components/__tests__/AddressName.test.tsx
+++ src/components/__tests__/AddressName.test.tsx
@@ -1,24 +1,23 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import AddressName from '../AddressName';
 
 describe('AddressName', () => {
-  afterEach(cleanup);
-
-  test('should render component', () => {
+  it('should render component', () => {
     render(<AddressName address='' />);
     const AddressNameComponent = screen.getByTestId('AddressName-1');
     expect(AddressNameComponent).toBeInTheDocument();
   });
 
-  test('passed prop should behave as expected', () => {
+  it('passed prop should behave as expected', () => {
     const address = 'Jilly';
     render(<AddressName address={address} />);
     const AddressNameComponent = screen.getByTestId('AddressName-1');
     expect(AddressNameComponent).toHaveTextContent(address);
   });
 
-  test('matches snapshot', () => {
+  it('matches snapshot', () => {
     const address = 'Jilly';
     const tree = renderer.create(<AddressName address={address} />).toJSON();
     expect(tree).toMatchSnapshot();
