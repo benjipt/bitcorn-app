f629c6ded4553be45c056e220183a71f75fa3693
Benji Thompson <thompson.benji@gmail.com>
2023-05-15 00:20:35 -0400

    feat: render welcome modal in BitcornUI when user is new

--- src/components/BitcornUI.tsx
+++ src/components/BitcornUI.tsx
@@ -1,11 +1,13 @@
 // BitcornUI.tsx is a child component of App.tsx. It renders the AppBar, BalanceCard, SendCard, and ChartCard components.
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@/components/AppBar';
 import BalanceCard from '@/components/BalanceCard';
 import SendCard from '@/components/SendCard';
 import ChartCard from '@/components/ChartCard';
 import { postTransaction } from '@/utils/api';
-import { useAppSelector } from '@/store/store';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { ModalTypes, openModal } from '@/store/slices/uiSlice';
+import Modal from './Modal';
 
 interface BitcornUIProps {
   getData: (address: string) => void;
@@ -16,13 +18,23 @@ const BitcornUI = ({ getData }: BitcornUIProps) => {
     address,
     balance,
     balanceHistory: data,
-    // isNewUser,
+    isNewUser,
   } = useAppSelector(state => state.user);
+  const showWelcomeModal = useAppSelector(
+    state => state.ui.showModal == ModalTypes.WELCOME
+  );
+
+  const dispatch = useAppDispatch();
+  // Open welcome modal if user is new
+  useEffect(() => {
+    if (isNewUser) dispatch(openModal(ModalTypes.WELCOME));
+  }, [isNewUser, dispatch]);
 
   return (
     <div>
       <AppBar address={address} />
       <div className='container-fluid'>
+        {showWelcomeModal && <Modal />}
         <div className='row ps-3'>
           <div className='row col-lg-4 me-3'>
             <BalanceCard balance={balance} />
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,11 @@
 // src/store/rootReducer.ts
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import uiReducer from './slices/uiSlice';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  ui: uiReducer,
 });
 
 export default rootReducer;
