efc2fc414d29ec106d5fbb3a4c89f07f1e1b3ed8
Benji Thompson <thompson.benji@gmail.com>
2023-05-18 00:00:54 -0400

    feat: add test to check for welcome modal in BitcornUI

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,25 @@
 import '@testing-library/jest-dom/extend-expect';
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
 
+// Workaround for ResizeObserver error described here ~~>
+// https://github.com/maslianok/react-resize-detector/issues/145
+const { ResizeObserver } = window;
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = vi.fn().mockImplementation(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn(),
+  }));
+});
 afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  vi.restoreAllMocks();
+  // <~~ Workaround for ResizeObserver error
   cleanup();
 });
